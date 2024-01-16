5c609fc9724240180416a68dc345e0fbf9c7fd6d
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2024-01-16 12:01:18 -0300

    todo list service

--- angular-classes2/src/app/services/todo.service.ts
+++ angular-classes2/src/app/services/todo.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core';
+import { ITodo, TTodoCreateFormData } from '../interfaces/todo.interface';
 
 @Injectable({ providedIn: 'root' })
 export class TodoService {
-    
+    private todoList: ITodo[] = [];
+
+    getTodoList(){
+        return this.todoList;
+    }
+
+    addTodo(formData: TTodoCreateFormData){
+        const newTodo = { ...formData, id: crypto.randomUUID() };
+        this.todoList.push(newTodo);
+    }
+
+    removeTodo(removingId: string){
+        const index = this.todoList.findIndex(todo => todo.id === removingId);
+        this.todoList.splice(index, 1);
+    }
 }
